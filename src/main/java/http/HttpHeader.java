7a748f8b3e2caf606d1f9f5b5ef2d9f975c5027a
Jeremy Wagemans <jeremy.wagemans@me.com>
2016-12-13 18:32:33 +0000

    Add HttpHeader Class

--- src/main/java/http/HttpHeader.java
+++ src/main/java/http/HttpHeader.java
@@ -2,6 +2,18 @@ package http;
 
 public class HttpHeader {
 
+    /* Request & Response headers */
+
+    public static String CACHE_CONTROL = "Cache-Control";
+    public static String CONNECTION = "Connection";
+    public static String CONTENT_LENGTH = "Content-Length";
+    public static String CONTENT_MD5 = "Content-MD5";
+    public static String DATE = "Date";
+    public static String PRAGMA = "Pragma";
+    public static String UPGRADE = "Upgrade";
+    public static String VIA = "Via";
+    public static String WARNING = "Warning";
+
     /* Request headers */
 
     public static String ACCEPT = "Accept";
@@ -10,34 +22,57 @@ public class HttpHeader {
     public static String ACCEPT_LANGUAGE = "Accept-Language";
     public static String ACCEPT_DATETIME = "Accept-Datetime";
     public static String AUTHORIZATION = "Authorization";
-    public static String CACHE_CONTROL = "Cache-Control";
-    public static String CONNECTION = "Connection";
     public static String COOKIE = "Cookie";
-    public static String CONTENT_LENGTH = "Content-Length";
-    public static String CONTENT_MD5 = "Content-MD5";
-    public static String DATE = "";
-    public static String EXPECT = "";
-    public static String FORWARDED = "";
-    public static String FROM = "";
-    public static String HOST = "";
-    public static String IF_MATCH = "";
-    public static String IF_MODIFIED_SINCE = "";
-    public static String IF_NONE_MATCH = "";
-    public static String IF_RANGE = "";
-    public static String IF_UNMODIFIED_SINCE = "";
+    public static String EXPECT = "Expect";
+    public static String FORWARDED = "Forwarded";
+    public static String FROM = "From";
+    public static String HOST = "Host";
+    public static String IF_MATCH = "If-Match";
+    public static String IF_MODIFIED_SINCE = "If-Modified-Since";
+    public static String IF_NONE_MATCH = "If-None-Match";
+    public static String IF_RANGE = "If-Range";
+    public static String IF_UNMODIFIED_SINCE = "If-Unmodified-Since";
     public static String MAX_FORWARDS = "Max-Forwards";
     public static String ORIGIN = "Origin";
-    public static String PRAGMA = "Pragma";
     public static String PROXY_AUTHORIZATION = "Proxy-Authorization";
     public static String RANGE = "Range";
     public static String REFERER = "Referer";
     public static String TE = "TE";
     public static String USER_AGENT = "User-Agent";
-    public static String UPGRADE = "Upgrade";
-    public static String VIA = "Via";
-    public static String WARNING = "Warning";
+
 
     /* Response headers */
 
+    public static String ACCESS_CONTROL_ALLOW_ORIGIN = "Access-Control-Allow-Origin";
+    public static String ACCEPT_PATCH = "Accept-Patch";
+    public static String ACCEPT_RANGES = "Accept-Ranges";
+    public static String AGE = "Age";
+    public static String ALLOW = "Allow";
+    public static String ALT_SVC = "Alt-Svc";
+    public static String CONTENT_DISPOSITION = "Content-Disposition";
+    public static String CONTENT_LANGUAGE = "Content-Language";
+    public static String CONTENT_LOCATION = "Content-Location";
+    public static String CONTENT_RANGE = "Content-Range";
+    public static String CONTENT_TYPE = "Content-Type";
+    public static String ETAG = "ETag";
+    public static String EXPIRES = "Expires";
+    public static String LAST_MODIFIED = "Last-Modified";
+    public static String LINK = "Link";
+    public static String LOCATION = "Location";
+    public static String P3P = "P3P";
+    public static String PROXY_AUTHENTICATE = "Proxy-Authenticate";
+    public static String PUBLIC_KEY_PINS = "Public-Key-Pins";
+    public static String REFRESH = "Refresh";
+    public static String RETRY_AFTER = "Retry-After";
+    public static String SERVER = "Server";
+    public static String SET_COOKIE = "Set-Cookie";
+    public static String STATUS = "Status";
+    public static String STRICT_TRANSPORT_SECURITY = "Strict-Transport-Security";
+    public static String TRAILER = "Trailer";
+    public static String TRANSFER_ENCODING = "Transfer-Encoding";
+    public static String TSV = "TSV";
+    public static String VARY = "Vary";
+    public static String WWW_AUTHENTICATE = "WWW-Authenticate";
+    public static String X_FRAME_OPTIONS = "X-Frame-Options";
 
 }
